fe6590eb981c41f94b262757ca01dc3126921f1b
dspwithaheart <42099338+dspwithaheart@users.noreply.github.com>
2018-10-23 09:10:38 +0200

    Update LocalStreamer.py

--- LocalStreamer.py
+++ LocalStreamer.py
@@ -5,7 +5,7 @@ Created on Fri Aug 24 08:56:34 2018
 @author: ayadav
 """
 
-"""Example program to demonstrate how to send a multi-channel time series to
+"""Program to demonstrate how to send a multi-channel time series to
 LSL."""
 
 import time
@@ -16,35 +16,6 @@ import numpy as np
 
 
 from modules.read_csv import read
-
-
-
-############################################
-#                                          #
-#           VARIABLES INIT                 #
-#                                          #
-############################################
-# sampling frequency (how many samples per second)
-fs = 250.0
-
-# bandpass values
-lowcut = 1.0
-highcut = 50.0
-
-# bandstop values
-lowstop = 49.0
-highstop = 51.0
-
-# file with eeg data location
-eeg_file = 'sampleData/OpenBCI-RAW-2018-08-21_13-47-49.csv'
-#eeg_file = 'BlinkDemo.csv'
-# seconds analysed
-sec_beg = 100
-sec_end = 200
-sec_rng = sec_end-sec_beg
-
-# lower and higher range values
-rng = [sec_beg*int(fs), sec_end*int(fs)]
 
 ############################################
 #                                          #
@@ -59,11 +30,11 @@ rng = [sec_beg*int(fs), sec_end*int(fs)]
 #    csvfile.seek(0)
 #    reader = csv.reader(csvfile, dialect)
     
-
 data = read(
     eeg_file, delimiter=',', header=7, to_float=False, transpose=False,
     comas=False, mode='rt'
     )
+
 #data = read(
 #    eeg_file, delimiter=',', header=0, to_float=False, transpose=False,
 #    comas=False, mode='rt'
@@ -77,7 +48,7 @@ data = np.asanyarray(data[:12])
 data = data.astype(np.float)
 
 # first create a new stream info (here we set the name to BioSemi,
-# the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
+# the content-type to EEG, 8 channels, 250 Hz, and float-valued data) The
 # last value would be the serial number of the device or some other more or
 # less locally unique identifier for the stream as far as available (you
 # could also omit it but interrupted connections wouldn't auto-recover)
@@ -89,15 +60,12 @@ outlet = StreamOutlet(info)
 print("now sending data...")
 i=0
 while True:
-    
     mysample=[]
     # make a new random 8-channel sample; this is converted into a
     # pylsl.vectorf (the data type that is expected by push_sample)
     for j in range(1,data.__len__()-3):
         mysample.append(data[j][i])
-    #mysample = [data[1][i], data[1][i], data[1][i], data[1][i], data[1][i], data[1][i], data[1][i], data[1][i]]
     # now send it and wait for a bit
-#    mysample=[data[0][i]]
     outlet.push_sample(mysample)
     time.sleep(0.005)
     if i<data[0].__len__()-1:
